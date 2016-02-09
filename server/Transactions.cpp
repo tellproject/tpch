566d9846c9edb88881c837fa096afd460652ba14
braunl <braunl@inf.ethz.ch>
2016-02-09 16:13:30 +0100

    removed orderkey bug

--- server/Transactions.cpp
+++ server/Transactions.cpp
@@ -37,10 +37,11 @@ RF1Out Transactions::rf1(tell::db::Transaction &tx, const RF1In &in)
         auto lTable = lFuture.get();
         auto oTable = oFuture.get();
 
+        tell::db::Counter orderCounter(tx.getCounter("order_idx_counter"));
         tell::db::Counter lineitemCounter(tx.getCounter("lineitem_idx_counter"));
 
         for (auto &order: in.orders) {
-            tx.insert(oTable, tell::db::key_t{uint64_t(order.orderkey)},
+            tx.insert(oTable, tell::db::key_t{orderCounter.next()},
                     {{
                     {"o_orderkey", order.orderkey},
                     {"o_custkey", order.custkey},
